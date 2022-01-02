2d36b01a238d3ac32c8a38d328559386a799394e
daimaxiaxie <zhang971207@hotmail.com>
2022-01-02 11:06:06 +0800

    template v2

---
+++ cache/Cache.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lenovo on 2021/12/30.
+//
+
+#include "Cache.h"
---
+++ cache/Cache.h
@@ -0,0 +1,14 @@
+//
+// Created by lenovo on 2021/12/30.
+//
+
+#ifndef BLOCKCHAIN_DNS_CACHE_H
+#define BLOCKCHAIN_DNS_CACHE_H
+
+
+class Cache {
+
+};
+
+
+#endif //BLOCKCHAIN_DNS_CACHE_H
